a461a55e8d987fcb3df485a1bdeff4fa65556b21
liya <272465820@qq.com>
2021-09-04 16:54:46 +0800

    feat: 限制字数

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -4,32 +4,48 @@
       type="textarea"
       :rows="2"
       placeholder="输入想放大的字，更多功能，联系liya"
-      v-model="textarea">
+      v-model="textarea"
+    >
     </el-input>
-    <div class="show">
-      {{textarea}}
+    <div style="padding: 10px 0">
+      <el-button type="primary" icon="el-icon-minus" @click="minus"
+        >缩小</el-button
+      >
+      <el-button type="primary"
+        >放大<i class="el-icon-plus" @click="plus"></i
+      ></el-button>
+    </div>
+    <div class="show" :style="{ fontSize: fontSize + 'px' }">
+      {{ textarea }}
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
-      textarea: ''
-    }
+      textarea: "",
+      fontSize: 80,
+    };
   },
   mounted() {},
-  methods: {}
-}
+  methods: {
+    plus() {
+      this.fontSize = this.fontSize + 4;
+    },
+    minus() {
+      this.fontSize = this.fontSize - 4;
+    },
+  },
+};
 </script>
 <style lang="less">
-.home{
+.home {
   padding: 20px;
-  .show{
-    font-size: 74px;
+  .show {
+    font-weight: 500;
   }
 }
 </style>
